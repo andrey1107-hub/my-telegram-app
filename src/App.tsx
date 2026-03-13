fd9ae7aae6d4c622ebcce2c533b41a485ecfaee0
andrey1107-hub <borya.07@inbox.ru>
2026-03-13 16:36:56 +0300

    modified:   src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -55,7 +55,7 @@ export default function Board() {
 }
   return (
     <>
-    <div className="status">{status}</div>
+    <div className="status">{window.status}</div>
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
